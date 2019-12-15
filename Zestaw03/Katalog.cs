682d261db08b0942a1ce3c20cb0fa0e23a1222eb
tdbok90 <tdbok90@gmail.com>
2019-12-16 00:59:09 +0100

    Dodano klase Katalog

---
+++ Zestaw03/Katalog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zestaw03
+{
+    class  Katalog
+    {
+        private string dzialTematyczny;
+        List<Pozycja> pozycje = new List<Pozycja>;
+
+        public Katalog()
+        {
+        }
+
+        public Katalog(string dzialTermiczny)
+        {
+            this.dzialTematyczny = dzialTermiczny;
+        }
+
+        public void DodajPozycje(Pozycja pozycja)
+        {
+            pozycje.Add(pozycja);
+        }
+    }
+}
